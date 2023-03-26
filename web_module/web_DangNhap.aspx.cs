41b37915e366ed777be66bed49860565d5db7e88
dangthibichlai <119518565+dangthibichlai@users.noreply.github.com>
2023-03-26 22:37:22 +0700

    dang-nhap

--- web_module/web_DangNhap.aspx.cs
+++ web_module/web_DangNhap.aspx.cs
@@ -17,17 +17,29 @@ public partial class web_module_web_DangNhap : System.Web.UI.Page
 
     protected void btnDanhNhap_ServerClick(object sender, EventArgs e)
     {
-        cls_security md5 = new cls_security();
+       // cls_security md5 = new cls_security();
         var sdt = txt_SoDienThoai.Value.Trim();
         var mk = txt_taikhoan.Value.Trim();
-        var mkmd5 = md5.HashCode(mk);
+        //var mkmd5 = md5.HashCode(mk);
 
-        var getData = from kh in db.khachhangs
-                      where kh.sdt == sdt && kh.password == mkmd5
-                      select kh;
+        var getData = from u in db.tb_Users
+                      where (u.user_Password == mk && u.user_Email == sdt) || (u.user_Password == mk && u.user_Phone == sdt)
+                      select u;
         if(getData.Count() > 0)
         {
-            Response.Redirect("/web_TrangChu.aspx");
+            Response.Cookies["UserNail"].Expires = DateTime.Now.AddDays(365);
+            Response.Cookies["phone"].Value = txt_SoDienThoai.Value.ToString();
+            if (getData.FirstOrDefault().groupuser_id == 4)
+            {
+                 Response.Redirect("/web_TrangChu.aspx");
+            }
+            else
+            {
+                Response.Redirect("/Admin_Default.aspx");
+            }
+           
+
+           
         }
         else
         {
