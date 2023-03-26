6011475b2dfe51d30ed38020643378a48a2ab718
Huỳnh Quốc Toản <huynhquoctoan1642002@gmail.com>
2023-03-26 22:04:38 +0700

    Toan Create Admin_MasterPage

---
+++ Admin_MasterPage.master.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_MasterPage : System.Web.UI.MasterPage
+{
+    dbcsdlDataContext db = new dbcsdlDataContext();
+    public string adminName, count;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Cookies["UserName"] != null)
+        {
+            adminName = Request.Cookies["UserName"].Value;
+            //loadMenu();
+        }
+        else
+        {
+            Response.Redirect("/admin-login");
+        }
+    }
+
+    protected void rpModule_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+
+    }
+
+    protected void rpModule_Init(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void btnLogout_ServerClick(object sender, EventArgs e)
+    {
+
+    }
+}
