6b6995c885350d41d7ea1cb57185141a38ea65af
dangthibichlai <119518565+dangthibichlai@users.noreply.github.com>
2023-03-26 21:04:34 +0700

    Lai chuc nang dang-nhap

--- web_module/web_DangNhap.aspx.cs
+++ web_module/web_DangNhap.aspx.cs
@@ -7,8 +7,34 @@ using System.Web.UI.WebControls;
 
 public partial class web_module_web_DangNhap : System.Web.UI.Page
 {
+    dbcsdlDataContext db = new dbcsdlDataContext();
+    cls_Alert cls = new cls_Alert();
+
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+
+    protected void btnDanhNhap_ServerClick(object sender, EventArgs e)
+    {
+        cls_security md5 = new cls_security();
+        var sdt = txt_SoDienThoai.Value.Trim();
+        var mk = txt_taikhoan.Value.Trim();
+        var mkmd5 = md5.HashCode(mk);
+
+        var getData = from kh in db.khachhangs
+                      where kh.sdt == sdt && kh.password == mkmd5
+                      select kh;
+        if(getData.Count() > 0)
+        {
+            Response.Redirect("/web_TrangChu.aspx");
+        }
+        else
+        {
+            cls.alert_Error(Page, "Mật khẩu hoặc số điện thoại không đúng!", "");
+        }
+
+        
 
     }
 }
