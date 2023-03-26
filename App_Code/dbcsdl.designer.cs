555cb7831fbd0926ae29e4f4b4c712edb1e78486
Huỳnh Quốc Toản <huynhquoctoan1642002@gmail.com>
2023-03-26 20:57:17 +0700

    Toan Create DB User

--- App_Code/dbcsdl.designer.cs
+++ App_Code/dbcsdl.designer.cs
@@ -29,15 +29,15 @@ public partial class dbcsdlDataContext : System.Data.Linq.DataContext
 	
   #region Extensibility Method Definitions
   partial void OnCreated();
-  partial void Inserttb_SanPham(tb_SanPham instance);
-  partial void Updatetb_SanPham(tb_SanPham instance);
-  partial void Deletetb_SanPham(tb_SanPham instance);
-  partial void Insertkhachhang(khachhang instance);
-  partial void Updatekhachhang(khachhang instance);
-  partial void Deletekhachhang(khachhang instance);
+  partial void Inserttb_User(tb_User instance);
+  partial void Updatetb_User(tb_User instance);
+  partial void Deletetb_User(tb_User instance);
   partial void Inserttb_LoaiSanPham(tb_LoaiSanPham instance);
   partial void Updatetb_LoaiSanPham(tb_LoaiSanPham instance);
   partial void Deletetb_LoaiSanPham(tb_LoaiSanPham instance);
+  partial void Inserttb_SanPham(tb_SanPham instance);
+  partial void Updatetb_SanPham(tb_SanPham instance);
+  partial void Deletetb_SanPham(tb_SanPham instance);
   #endregion
 	
 	public dbcsdlDataContext() : 
@@ -70,19 +70,19 @@ public partial class dbcsdlDataContext : System.Data.Linq.DataContext
 		OnCreated();
 	}
 	
-	public System.Data.Linq.Table<tb_SanPham> tb_SanPhams
+	public System.Data.Linq.Table<admin_GroupUser> admin_GroupUsers
 	{
 		get
 		{
-			return this.GetTable<tb_SanPham>();
+			return this.GetTable<admin_GroupUser>();
 		}
 	}
 	
-	public System.Data.Linq.Table<khachhang> khachhangs
+	public System.Data.Linq.Table<tb_User> tb_Users
 	{
 		get
 		{
-			return this.GetTable<khachhang>();
+			return this.GetTable<tb_User>();
 		}
 	}
 	
@@ -93,192 +93,81 @@ public partial class dbcsdlDataContext : System.Data.Linq.DataContext
 			return this.GetTable<tb_LoaiSanPham>();
 		}
 	}
+	
+	public System.Data.Linq.Table<tb_SanPham> tb_SanPhams
+	{
+		get
+		{
+			return this.GetTable<tb_SanPham>();
+		}
+	}
 }
 
-[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.tb_SanPham")]
-public partial class tb_SanPham : INotifyPropertyChanging, INotifyPropertyChanged
+[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.admin_GroupUser")]
+public partial class admin_GroupUser
 {
 	
-	private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+	private int _groupuser_id;
 	
-	private int _sanpham_id;
+	private string _groupuser_name;
 	
-	private string _sanpham_name;
+	private System.Nullable<bool> _groupuser_active;
 	
-	private System.Nullable<bool> _sanpham_new;
-	
-	private string _sanpham_image;
-	
-	private int _loaisanpham_id;
-	
-	private System.Nullable<int> _sanpham_luotmua;
-	
-    #region Extensibility Method Definitions
-    partial void OnLoaded();
-    partial void OnValidate(System.Data.Linq.ChangeAction action);
-    partial void OnCreated();
-    partial void Onsanpham_idChanging(int value);
-    partial void Onsanpham_idChanged();
-    partial void Onsanpham_nameChanging(string value);
-    partial void Onsanpham_nameChanged();
-    partial void Onsanpham_newChanging(System.Nullable<bool> value);
-    partial void Onsanpham_newChanged();
-    partial void Onsanpham_imageChanging(string value);
-    partial void Onsanpham_imageChanged();
-    partial void Onloaisanpham_idChanging(int value);
-    partial void Onloaisanpham_idChanged();
-    partial void Onsanpham_luotmuaChanging(System.Nullable<int> value);
-    partial void Onsanpham_luotmuaChanged();
-    #endregion
-	
-	public tb_SanPham()
+	public admin_GroupUser()
 	{
-		OnCreated();
 	}
 	
-	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_id", DbType="Int NOT NULL", IsPrimaryKey=true)]
-	public int sanpham_id
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_groupuser_id", AutoSync=AutoSync.Always, DbType="Int NOT NULL IDENTITY", IsDbGenerated=true)]
+	public int groupuser_id
 	{
 		get
 		{
-			return this._sanpham_id;
+			return this._groupuser_id;
 		}
 		set
 		{
-			if ((this._sanpham_id != value))
+			if ((this._groupuser_id != value))
 			{
-				this.Onsanpham_idChanging(value);
-				this.SendPropertyChanging();
-				this._sanpham_id = value;
-				this.SendPropertyChanged("sanpham_id");
-				this.Onsanpham_idChanged();
+				this._groupuser_id = value;
 			}
 		}
 	}
 	
-	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_name", DbType="NChar(10)")]
-	public string sanpham_name
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_groupuser_name", DbType="NVarChar(MAX)")]
+	public string groupuser_name
 	{
 		get
 		{
-			return this._sanpham_name;
+			return this._groupuser_name;
 		}
 		set
 		{
-			if ((this._sanpham_name != value))
+			if ((this._groupuser_name != value))
 			{
-				this.Onsanpham_nameChanging(value);
-				this.SendPropertyChanging();
-				this._sanpham_name = value;
-				this.SendPropertyChanged("sanpham_name");
-				this.Onsanpham_nameChanged();
+				this._groupuser_name = value;
 			}
 		}
 	}
 	
-	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_new", DbType="Bit")]
-	public System.Nullable<bool> sanpham_new
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_groupuser_active", DbType="Bit")]
+	public System.Nullable<bool> groupuser_active
 	{
 		get
 		{
-			return this._sanpham_new;
+			return this._groupuser_active;
 		}
 		set
 		{
-			if ((this._sanpham_new != value))
+			if ((this._groupuser_active != value))
 			{
-				this.Onsanpham_newChanging(value);
-				this.SendPropertyChanging();
-				this._sanpham_new = value;
-				this.SendPropertyChanged("sanpham_new");
-				this.Onsanpham_newChanged();
+				this._groupuser_active = value;
 			}
-		}
-	}
-	
-	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_image", DbType="NVarChar(MAX) NOT NULL", CanBeNull=false)]
-	public string sanpham_image
-	{
-		get
-		{
-			return this._sanpham_image;
-		}
-		set
-		{
-			if ((this._sanpham_image != value))
-			{
-				this.Onsanpham_imageChanging(value);
-				this.SendPropertyChanging();
-				this._sanpham_image = value;
-				this.SendPropertyChanged("sanpham_image");
-				this.Onsanpham_imageChanged();
-			}
-		}
-	}
-	
-	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_loaisanpham_id", DbType="Int NOT NULL")]
-	public int loaisanpham_id
-	{
-		get
-		{
-			return this._loaisanpham_id;
-		}
-		set
-		{
-			if ((this._loaisanpham_id != value))
-			{
-				this.Onloaisanpham_idChanging(value);
-				this.SendPropertyChanging();
-				this._loaisanpham_id = value;
-				this.SendPropertyChanged("loaisanpham_id");
-				this.Onloaisanpham_idChanged();
-			}
-		}
-	}
-	
-	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_luotmua", DbType="Int")]
-	public System.Nullable<int> sanpham_luotmua
-	{
-		get
-		{
-			return this._sanpham_luotmua;
-		}
-		set
-		{
-			if ((this._sanpham_luotmua != value))
-			{
-				this.Onsanpham_luotmuaChanging(value);
-				this.SendPropertyChanging();
-				this._sanpham_luotmua = value;
-				this.SendPropertyChanged("sanpham_luotmua");
-				this.Onsanpham_luotmuaChanged();
-			}
-		}
-	}
-	
-	public event PropertyChangingEventHandler PropertyChanging;
-	
-	public event PropertyChangedEventHandler PropertyChanged;
-	
-	protected virtual void SendPropertyChanging()
-	{
-		if ((this.PropertyChanging != null))
-		{
-			this.PropertyChanging(this, emptyChangingEventArgs);
-		}
-	}
-	
-	protected virtual void SendPropertyChanged(String propertyName)
-	{
-		if ((this.PropertyChanged != null))
-		{
-			this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
 
-[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.khachhang")]
-public partial class khachhang : INotifyPropertyChanging, INotifyPropertyChanged
+[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.tb_User")]
+public partial class tb_User : INotifyPropertyChanging, INotifyPropertyChanged
 {
 	
 	private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
@@ -309,7 +198,7 @@ public partial class khachhang : INotifyPropertyChanging, INotifyPropertyChanged
     partial void OnpasswordChanged();
     #endregion
 	
-	public khachhang()
+	public tb_User()
 	{
 		OnCreated();
 	}
@@ -544,6 +433,188 @@ public partial class tb_LoaiSanPham : INotifyPropertyChanging, INotifyPropertyCh
 				this._loaisanpham_image = value;
 				this.SendPropertyChanged("loaisanpham_image");
 				this.Onloaisanpham_imageChanged();
+			}
+		}
+	}
+	
+	public event PropertyChangingEventHandler PropertyChanging;
+	
+	public event PropertyChangedEventHandler PropertyChanged;
+	
+	protected virtual void SendPropertyChanging()
+	{
+		if ((this.PropertyChanging != null))
+		{
+			this.PropertyChanging(this, emptyChangingEventArgs);
+		}
+	}
+	
+	protected virtual void SendPropertyChanged(String propertyName)
+	{
+		if ((this.PropertyChanged != null))
+		{
+			this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
+
+[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.tb_SanPham")]
+public partial class tb_SanPham : INotifyPropertyChanging, INotifyPropertyChanged
+{
+	
+	private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+	
+	private int _sanpham_id;
+	
+	private string _sanpham_name;
+	
+	private System.Nullable<bool> _sanpham_new;
+	
+	private string _sanpham_image;
+	
+	private int _loaisanpham_id;
+	
+	private System.Nullable<int> _sanpham_luotmua;
+	
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void Onsanpham_idChanging(int value);
+    partial void Onsanpham_idChanged();
+    partial void Onsanpham_nameChanging(string value);
+    partial void Onsanpham_nameChanged();
+    partial void Onsanpham_newChanging(System.Nullable<bool> value);
+    partial void Onsanpham_newChanged();
+    partial void Onsanpham_imageChanging(string value);
+    partial void Onsanpham_imageChanged();
+    partial void Onloaisanpham_idChanging(int value);
+    partial void Onloaisanpham_idChanged();
+    partial void Onsanpham_luotmuaChanging(System.Nullable<int> value);
+    partial void Onsanpham_luotmuaChanged();
+    #endregion
+	
+	public tb_SanPham()
+	{
+		OnCreated();
+	}
+	
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_id", DbType="Int NOT NULL", IsPrimaryKey=true)]
+	public int sanpham_id
+	{
+		get
+		{
+			return this._sanpham_id;
+		}
+		set
+		{
+			if ((this._sanpham_id != value))
+			{
+				this.Onsanpham_idChanging(value);
+				this.SendPropertyChanging();
+				this._sanpham_id = value;
+				this.SendPropertyChanged("sanpham_id");
+				this.Onsanpham_idChanged();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_name", DbType="NChar(10)")]
+	public string sanpham_name
+	{
+		get
+		{
+			return this._sanpham_name;
+		}
+		set
+		{
+			if ((this._sanpham_name != value))
+			{
+				this.Onsanpham_nameChanging(value);
+				this.SendPropertyChanging();
+				this._sanpham_name = value;
+				this.SendPropertyChanged("sanpham_name");
+				this.Onsanpham_nameChanged();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_new", DbType="Bit")]
+	public System.Nullable<bool> sanpham_new
+	{
+		get
+		{
+			return this._sanpham_new;
+		}
+		set
+		{
+			if ((this._sanpham_new != value))
+			{
+				this.Onsanpham_newChanging(value);
+				this.SendPropertyChanging();
+				this._sanpham_new = value;
+				this.SendPropertyChanged("sanpham_new");
+				this.Onsanpham_newChanged();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_image", DbType="NVarChar(MAX) NOT NULL", CanBeNull=false)]
+	public string sanpham_image
+	{
+		get
+		{
+			return this._sanpham_image;
+		}
+		set
+		{
+			if ((this._sanpham_image != value))
+			{
+				this.Onsanpham_imageChanging(value);
+				this.SendPropertyChanging();
+				this._sanpham_image = value;
+				this.SendPropertyChanged("sanpham_image");
+				this.Onsanpham_imageChanged();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_loaisanpham_id", DbType="Int NOT NULL")]
+	public int loaisanpham_id
+	{
+		get
+		{
+			return this._loaisanpham_id;
+		}
+		set
+		{
+			if ((this._loaisanpham_id != value))
+			{
+				this.Onloaisanpham_idChanging(value);
+				this.SendPropertyChanging();
+				this._loaisanpham_id = value;
+				this.SendPropertyChanged("loaisanpham_id");
+				this.Onloaisanpham_idChanged();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_sanpham_luotmua", DbType="Int")]
+	public System.Nullable<int> sanpham_luotmua
+	{
+		get
+		{
+			return this._sanpham_luotmua;
+		}
+		set
+		{
+			if ((this._sanpham_luotmua != value))
+			{
+				this.Onsanpham_luotmuaChanging(value);
+				this.SendPropertyChanging();
+				this._sanpham_luotmua = value;
+				this.SendPropertyChanged("sanpham_luotmua");
+				this.Onsanpham_luotmuaChanged();
 			}
 		}
 	}
